0e72db2e4a9f67d31418c0adedfd87358a5ddfdd
Ivan <ivansakal2@gmail.com>
2019-11-21 18:53:00 +0100

    animation earth 2

--- gui/src/main/java/org/sample/checkers/earth/Earth.java
+++ gui/src/main/java/org/sample/checkers/earth/Earth.java
@@ -1,16 +1,20 @@
 package org.sample.checkers.earth;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.*;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Transform;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 public class Earth extends Application {
@@ -27,17 +31,27 @@ public class Earth extends Application {
     private DoubleProperty angleX = new SimpleDoubleProperty(0);
     private DoubleProperty angleY = new SimpleDoubleProperty(0);
 
+    private final Sphere sphere = new Sphere(150);
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Camera camera = new PerspectiveCamera(true);
         camera.setNearClip(1);
-        camera.setFarClip(1000);
+        camera.setFarClip(10000);
         camera.translateZProperty().set(-1000);
 
         SmartGroup earth = new SmartGroup();
         earth.getChildren().add(prepareEarth());
 
-        Scene scene = new Scene(earth, WIDTH, HEIGHT, true);
+        Slider slider = prepareSlider();
+        earth.translateZProperty().bind(slider.valueProperty());
+
+        SmartGroup root = new SmartGroup();
+        root.getChildren().add(earth);
+        root.getChildren().add(prepareImageView());
+        root.getChildren().add(slider);
+
+        Scene scene = new Scene(root, WIDTH, HEIGHT, true);
         scene.setFill(Color.SILVER);
         scene.setCamera(camera);
 
@@ -46,6 +60,40 @@ public class Earth extends Application {
         primaryStage.setTitle("Earth");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        prepareAnimation();
+    }
+
+    private void prepareAnimation() {
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                sphere.rotateProperty().set(sphere.getRotate() + 0.2);
+            }
+        };
+        timer.start();
+    }
+
+    private ImageView prepareImageView() {
+        Image image = new Image(getClass().getResourceAsStream("galaxy_background_1920X1080.jpg"));
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.setSmooth(false);
+        imageView.getTransforms().add(new Translate(-image.getWidth() / 2, -image.getHeight() / 2, 800));
+        return imageView;
+    }
+
+    private Slider prepareSlider() {
+        Slider slider = new Slider();
+        slider.setMax(800);
+        slider.setMin(-400);
+        slider.setPrefWidth(300d);
+        slider.setLayoutX(-150);
+        slider.setLayoutY(200);
+        slider.setShowTickLabels(true);
+        slider.setTranslateZ(5);
+        slider.setStyle("-fx-base: black");
+        return slider;
     }
 
     private Node prepareEarth() {
@@ -54,7 +102,8 @@ public class Earth extends Application {
         earthMaterial.setSelfIlluminationMap(new Image(getClass().getResourceAsStream("earth_s2.jpg")));
         earthMaterial.setSpecularMap(new Image(getClass().getResourceAsStream("earth_r.png")));
         earthMaterial.setBumpMap(new Image(getClass().getResourceAsStream("earth_normalmap_flat_8192x4096.jpg")));
-        Sphere sphere = new Sphere(150);
+
+        sphere.setRotationAxis(Rotate.Y_AXIS);
         sphere.setMaterial(earthMaterial);
         return sphere;
     }
