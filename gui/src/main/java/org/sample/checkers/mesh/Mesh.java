0a43c10f7748724e871527de902fd42fa7420f34
Ivan <ivansakal2@gmail.com>
2019-11-23 10:36:38 +0100

    start mesh

---
+++ gui/src/main/java/org/sample/checkers/mesh/Mesh.java
@@ -0,0 +1,95 @@
+package org.sample.checkers.mesh;
+
+import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.Camera;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
+import org.sample.checkers.mesh.components.SmartGroup;
+
+public class Mesh extends Application {
+
+    private static final float WIDTH = 800;
+    private static final float HEIGHT = 600;
+
+    private double anchorX;
+    private double anchorY;
+
+    private double anchorAngleX = 0;
+    private double anchorAngleY = 0;
+
+    private DoubleProperty angleX = new SimpleDoubleProperty(0);
+    private DoubleProperty angleY = new SimpleDoubleProperty(0);
+
+    @Override
+    public void start(Stage primaryStage) {
+        SmartGroup group = new SmartGroup();
+
+        Camera camera = new PerspectiveCamera(true);
+        camera.setNearClip(1);
+        camera.setFarClip(10000);
+        camera.translateZProperty().set(-1000);
+
+        Cylinder sphere = new Cylinder(50, 100);
+        group.getChildren().add(sphere);
+
+        Scene root = new Scene(group, WIDTH, HEIGHT, true, SceneAntialiasing.DISABLED);
+        root.setFill(Color.SILVER);
+        root.setCamera(camera);
+
+        initMouseControl(group, root, primaryStage);
+
+        primaryStage.setTitle("Mesh");
+        primaryStage.setScene(root);
+        primaryStage.show();
+    }
+
+    private void initMouseControl(SmartGroup group, Scene boardScene, Stage stage) {
+        Rotate xRotate;
+        Rotate yRotate;
+
+        group.getTransforms().addAll(
+                xRotate = new Rotate(0, Rotate.X_AXIS),
+                yRotate = new Rotate(0, Rotate.Y_AXIS)
+        );
+
+        xRotate.angleProperty().bind(angleX);
+        yRotate.angleProperty().bind(angleY);
+
+        boardScene.setOnMousePressed(event -> {
+            anchorX = event.getSceneX();
+            anchorY = event.getSceneY();
+            anchorAngleX = angleX.get();
+            anchorAngleY = angleY.get();
+        });
+
+        boardScene.setOnMouseDragged(event -> {
+            angleX.set(anchorAngleX - (anchorY - event.getSceneY()));
+            angleY.set(anchorAngleY - (anchorX - event.getSceneX()));
+        });
+
+        stage.addEventHandler(ScrollEvent.SCROLL, event -> {
+            double delta = event.getDeltaY();
+            group.translateZProperty().set(group.getTranslateZ() + delta);
+        });
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX
+     * application. main() serves only as fallback in case the
+     * application can not be launched through deployment artifacts,
+     * e.g., in IDEs with limited FX support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
