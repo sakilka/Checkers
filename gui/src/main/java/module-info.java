b8cabbb1a2492696693f7d6c105e7867addb4f01
Ivan <ivansakal2@gmail.com>
2019-11-23 10:14:47 +0100

    refactor

--- gui/src/main/java/module-info.java
+++ gui/src/main/java/module-info.java
@@ -5,7 +5,9 @@ module hellofx {
     opens org.sample.checkers to javafx.fxml;
     opens org.sample.checkers.controller to javafx.fxml;
     opens org.sample.checkers.earth to javafx.fxml;
+    opens org.sample.checkers.basic to javafx.fxml;
     exports org.sample.checkers;
     exports org.sample.checkers.controller;
     exports org.sample.checkers.earth;
+    exports org.sample.checkers.basic;
 }
