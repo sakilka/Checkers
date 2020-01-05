c4eae6024eeebd7a85b3ec8d6ac51d0de3eb1019
Ivan <ivansakal2@gmail.com>
2020-01-05 22:25:02 +0100

    complete figures

--- gui/src/main/java/org/sample/checkers/board/FigureMain.java
+++ gui/src/main/java/org/sample/checkers/board/FigureMain.java
@@ -11,9 +11,9 @@ import javafx.scene.SceneAntialiasing;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
-import org.sample.checkers.board.model.Cube;
 import org.sample.checkers.board.model.LoadObject;
 import org.sample.checkers.mesh.components.SmartGroup;
 
@@ -40,15 +40,50 @@ public class FigureMain extends Application {
         Camera camera = new PerspectiveCamera(true);
         camera.setNearClip(1);
         camera.setFarClip(10000);
-        camera.translateZProperty().set(-1500);
+        camera.translateZProperty().set(-150);
 
-        Cube cube = new Cube(100, 100, 100);
+//        Cube cube = new Cube(100, 100, 100);
+//
+//        cube.setTranslateX(0);
+//        cube.setTranslateY(0);
+//        cube.setTranslateZ(0);
+//        cube.setMaterial(new PhongMaterial(Color.GREEN));
 
-        cube.setTranslateX(0);
-        cube.setTranslateY(0);
-        cube.setTranslateZ(0);
-        cube.setMaterial(new PhongMaterial(Color.GREEN));
+        Box board = new Box(5, 1, 5);
+        board.setTranslateX(0);
+        board.setTranslateY(0.5);
+        board.setTranslateZ(0);
+        board.setMaterial(new PhongMaterial(Color.BLACK));
 
+        Box board2 = new Box(5, 1, 5);
+        board2.setTranslateX(-5);
+        board2.setTranslateY(0.5);
+        board2.setTranslateZ(0);
+        board2.setMaterial(new PhongMaterial(Color.WHITE));
+
+        Box board3 = new Box(5, 1, 5);
+        board3.setTranslateX(-10);
+        board3.setTranslateY(0.5);
+        board3.setTranslateZ(0);
+        board3.setMaterial(new PhongMaterial(Color.BLACK));
+
+        Box board4 = new Box(5, 1, 5);
+        board4.setTranslateX(-15);
+        board4.setTranslateY(0.5);
+        board4.setTranslateZ(0);
+        board4.setMaterial(new PhongMaterial(Color.WHITE));
+
+        Box board5 = new Box(5, 1, 5);
+        board5.setTranslateX(-20);
+        board5.setTranslateY(0.5);
+        board5.setTranslateZ(0);
+        board5.setMaterial(new PhongMaterial(Color.BLACK));
+
+        Box board6 = new Box(5, 1, 5);
+        board6.setTranslateX(-25);
+        board6.setTranslateY(0.5);
+        board6.setTranslateZ(0);
+        board6.setMaterial(new PhongMaterial(Color.WHITE));
 //        Figure figure = new Figure(250, 10);
 //
 //        figure.setTranslateX(0);
@@ -56,39 +91,45 @@ public class FigureMain extends Application {
 //        figure.setTranslateZ(0);
 //        figure.setMaterial(new PhongMaterial(Color.RED));
 
-        LoadObject pawn = new LoadObject(new File((getClass().getResource("pawn.obj").getFile())), 10);
+        LoadObject pawn = new LoadObject(new File((getClass().getResource("pawn.obj").getFile())), 1);
         pawn.setMaterial(new PhongMaterial(Color.WHITE));
         pawn.setTranslateX(0);
         pawn.setTranslateY(0);
         pawn.setTranslateZ(0);
 
-        LoadObject knight = new LoadObject(new File((getClass().getResource("knight.obj").getFile())), 10);
+        LoadObject knight = new LoadObject(new File((getClass().getResource("knight.obj").getFile())), 1);
         knight.setMaterial(new PhongMaterial(Color.DARKGOLDENROD));
-        knight.setTranslateX(-50);
+        knight.setTranslateX(-5);
         knight.setTranslateY(0);
         knight.setTranslateZ(0);
 
-        LoadObject queen = new LoadObject(new File((getClass().getResource("queen.obj").getFile())), 10);
-        queen.setMaterial(new PhongMaterial(Color.WHITE));
-        queen.setTranslateX(-100);
+        LoadObject queen = new LoadObject(new File((getClass().getResource("queen.obj").getFile())), 1);
+        PhongMaterial silver = new PhongMaterial(Color.SILVER);
+        silver.setSpecularColor(Color.WHITE);
+        silver.setSpecularPower(15);
+        queen.setMaterial(silver);
+        queen.setTranslateX(-10);
         queen.setTranslateY(0);
         queen.setTranslateZ(0);
 
-        LoadObject king = new LoadObject(new File((getClass().getResource("king.obj").getFile())), 10);
-        king.setMaterial(new PhongMaterial(Color.DARKGOLDENROD));
-        king.setTranslateX(-150);
+        LoadObject king = new LoadObject(new File((getClass().getResource("king.obj").getFile())), 1);
+        PhongMaterial gold = new PhongMaterial(Color.GOLD);
+        gold.setSpecularColor(Color.WHITE);
+        gold.setSpecularPower(15);
+        king.setMaterial(gold);
+        king.setTranslateX(-15);
         king.setTranslateY(0);
         king.setTranslateZ(0);
 
-        LoadObject bishop = new LoadObject(new File((getClass().getResource("bishop.obj").getFile())), 10);
+        LoadObject bishop = new LoadObject(new File((getClass().getResource("bishop.obj").getFile())), 1);
         bishop.setMaterial(new PhongMaterial(Color.WHITE));
-        bishop.setTranslateX(-200);
+        bishop.setTranslateX(-20);
         bishop.setTranslateY(0);
         bishop.setTranslateZ(0);
 
-        LoadObject rook = new LoadObject(new File((getClass().getResource("rook.obj").getFile())), 10);
+        LoadObject rook = new LoadObject(new File((getClass().getResource("rook.obj").getFile())), 1);
         rook.setMaterial(new PhongMaterial(Color.DARKGOLDENROD));
-        rook.setTranslateX(-250);
+        rook.setTranslateX(-25);
         rook.setTranslateY(0);
         rook.setTranslateZ(0);
 //        LoadObject loadObject = new LoadObject(new File((getClass().getResource("pawn2.obj").getFile())), 100);
@@ -97,7 +138,12 @@ public class FigureMain extends Application {
 ////        whiteMaterial.setSpecularColor(Color.BLACK);
 //        loadObject.setMaterial(new PhongMaterial(Color.WHITE));
 
-        group.getChildren().add(cube);
+        group.getChildren().add(board);
+        group.getChildren().add(board2);
+        group.getChildren().add(board3);
+        group.getChildren().add(board4);
+        group.getChildren().add(board5);
+        group.getChildren().add(board6);
         group.getChildren().add(pawn);
         group.getChildren().add(knight);
         group.getChildren().add(queen);
