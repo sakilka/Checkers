c614e9ce3f495ce2201ba0bb7f4296296233eec8
Ivan <ivansakal2@gmail.com>
2022-09-15 18:16:18 +0200

    load model improve

--- common/src/main/java/org/sample/checkers/config/LoadModel.java
+++ common/src/main/java/org/sample/checkers/config/LoadModel.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
